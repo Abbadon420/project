72d8f4d6ddb46d549f81766f38af54ada8ae1cc6
Maniz KC <100184084+Abbadon420@users.noreply.github.com>
2022-07-24 11:01:34 +0545

    Add files via upload

--- New folder/script.js
+++ New folder/script.js
@@ -77,12 +77,6 @@ var responsiveSlider = function() {
  }
 ;
  /*extra code*/
- function myMap() {
-  var mapProp= {
-    center:new google.maps.LatLng(51.508742,-0.120850),
-    zoom:5,
-  };
-  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-  }
+
 
                                           
